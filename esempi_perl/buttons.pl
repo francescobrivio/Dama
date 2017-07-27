ddb1c0cb4b2de20c6c0f89adafb03a8e32205132
Francesco Brivio <francescobrivio@MacBook-Pro-di-Francesco.local>
2017-07-27 17:01:53 +0200

    first perl-tk example

---
+++ esempi_perl/buttons.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+use Tk;
+
+my $mw = MainWindow->new;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+MainLoop;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sub BEGIN
+{
+	$val = 2;
+	print "Initializing val to $val\n";
+}
+
