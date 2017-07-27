19df5d23231788246ce837b6b69d254d0c1e90a2
Francesco Brivio <francescobrivio@MacBook-Pro-di-Francesco.local>
2017-07-27 18:27:55 +0200

    esempio milgliorato; link utili

--- esempi_perl/buttons.pl
+++ esempi_perl/buttons.pl
@@ -3,39 +3,39 @@
 use Tk;
 
 my $mw = MainWindow->new;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$mw->geometry("250x140");
+$mw->title ("A first Tk Demo");
+$mw->Label(
+            -text => "Some text"
+          )->pack;
+$mw->Button(
+            -text    => "Print a Value",
+            -command => \&printText
+           )->pack;
+$mw->Button(
+            -text    => "Double the Value",
+            -command => sub {$val *= 2}
+           )->pack;
+$mw->Button(
+            -text    => "Quit",
+            -command => \&finish
+           )->pack;
 
 MainLoop;
 
 
+sub printText
+{
+    print "Value is $val\n";
+}
 
 
-
-
-
-
-
-
-
-
-
-
+sub finish
+{
+    print "Quitting...\n";
+    print "Final Value is $val\n";
+    exit(0);
+}
 
 sub BEGIN
 {
